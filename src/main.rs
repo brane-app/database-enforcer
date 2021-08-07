4916cc345ddbbac8ba1d71a4354c33bcaf3cfa5a
Eva Harris <mail@gastrodon.io>
2021-08-07 01:29:17 -0400

    Create enforce_all main loop
    Create an enforce_all function to act as a main loop for enforcing the
    schemas that we've defined

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,27 @@
-const FREQUENCY: usize = 10_000;
+#![feature(async_closure)]
+
+mod schema;
+
+use futures::join;
+use schema::{load_schema, Table};
+use std::time::Duration;
+use tokio::time::sleep;
+
+const FREQUENCY: Duration = Duration::from_millis(10_000);
+
+async fn enforce_all(tables: Vec<Table>) {
+    loop {
+        for table in &tables {
+            unimplemented!();
+        }
+
+        sleep(FREQUENCY).await;
+    }
+}
 
 #[tokio::main]
-async fn main() -> () {
+async fn main() {
+    join!(enforce_all(load_schema("./schema")));
+
     println!("Hello, async");
 }
