5285e44420b57bc14ffcd3c418307b8cc45f0f09
Eva Harris <mail@gastrodon.io>
2021-08-07 04:11:13 -0400

    Restructure enforcing
    loop_enforce_all calls enforce on every table directly, so enforce_all
    was dropped
    
    Make sure that sleeping is done INSIDE the loop
    
    Get a connection string form the environment and create a pool with it,
    accept a reference to a pool in methods that enfoce database things

--- src/main.rs
+++ src/main.rs
@@ -3,27 +3,35 @@
 mod schema;
 
 use futures::join;
+use mysql::{Opts, Pool};
 use schema::{load_schema, Table};
-use std::time::Duration;
+use std::{env::var, time::Duration};
 use tokio::time::sleep;
 
 const FREQUENCY: Duration = Duration::from_millis(10_000);
+static SCHEMA_ROOT: &str = "./schema";
 
-async fn enforce_all(tables: Vec<Table>) {
+async fn enforce(pool: &Pool, table: Table) {
     unimplemented!();
 }
 
-async fn loop_enforce_all() {
+async fn loop_enforce_all(pool: &Pool) {
     loop {
-        enforce_all(load_schema("./schema")).await
-    }
+        for table in load_schema(SCHEMA_ROOT).iter() {
+            enforce(pool, table).await
+        }
 
-    sleep(FREQUENCY).await;
+        sleep(FREQUENCY).await;
+    }
 }
 
 #[tokio::main]
 async fn main() {
-    join!(loop_enforce_all());
+    let address = var("DATABASE_CONNECTION").expect("DATABASE_CONNECTION missing!");
+    let pool = Pool::new(Opts::from_url(&address).expect("DATABASE_CONNECTION isn't valid!"))
+        .unwrap_or_else(|err| panic!("{}", err));
+
+    join!(loop_enforce_all(&pool));
 
     println!("Hello, async");
 }
