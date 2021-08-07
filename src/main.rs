319eb63aef744ac4a49b4e55deffee8ace5ba320
Eva Harris <mail@gastrodon.io>
2021-08-07 04:58:40 -0400

    Do sql
    Require the mysql library
    
    Actually execute mysql stuff to create desired tables if they don't
    exist

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@
 mod schema;
 
 use futures::join;
+use mysql::prelude::Queryable;
 use mysql::{Opts, Pool};
 use schema::{load_schema, Table};
 use std::{env::var, time::Duration};
@@ -11,14 +12,23 @@ use tokio::time::sleep;
 const FREQUENCY: Duration = Duration::from_millis(10_000);
 static SCHEMA_ROOT: &str = "./schema";
 
-async fn enforce(pool: &Pool, table: Table) {
-    unimplemented!();
+async fn enforce(pool: &Pool, table: &Table) -> mysql::Result<()> {
+    let fields_join = table.field_schemas.join(", ");
+    let statement = format!(
+        "CREATE TABLE IF NOT EXISTS {name} ({fields})",
+        name = table.name,
+        fields = fields_join
+    );
+
+    pool.get_conn()?.query_drop(statement)
 }
 
 async fn loop_enforce_all(pool: &Pool) {
     loop {
         for table in load_schema(SCHEMA_ROOT).iter() {
-            enforce(pool, table).await
+            if let Err(why) = enforce(pool, table).await {
+                println!("Failed to enforce {}: {}", table.name, why);
+            };
         }
 
         sleep(FREQUENCY).await;
